2ccf3babe66864752a107cc7d5deac42f22a5c0f
Abhishek Jandial <abshk12@gmail.com>
2022-04-05 12:24:39 +0200

    adding left right car rotation keys

--- Python_project/CarSimulator/simulator.py
+++ Python_project/CarSimulator/simulator.py
@@ -4,11 +4,11 @@ import math
 import sys
 import neat
 
-SCREEN_WIDTH = 1080
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1244
+SCREEN_HEIGHT = 1016
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-TRACK = pygame.image.load(os.path.join( "./Piste_image.jpg"))
+TRACK = pygame.image.load(os.path.join( "./Piste_image.png"))
 
 
 class Vehicle(pygame.sprite.Sprite):
@@ -20,6 +20,8 @@ class Vehicle(pygame.sprite.Sprite):
         self.drive_state = False
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
+        self.rotation_vel = 5
+        self.direction = 0
 
     def update(self):
         self.drive()
@@ -30,7 +32,15 @@ class Vehicle(pygame.sprite.Sprite):
             self.rect.center += self.vel_vector * 6
     
     def rotate(self):
+        if self.direction == 1:
+            self.angle -= self.rotation_vel
+            self.vel_vector.rotate_ip(self.rotation_vel)
+        if self.direction == -1:
+            self.angle += self.rotation_vel
+            self.vel_vector.rotate_ip(-self.rotation_vel)
+
         self.image = pygame.transform.rotozoom(self.orignial_image, self.angle, 0.1)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 vehicle = pygame.sprite.GroupSingle(Vehicle())
 
@@ -48,10 +58,20 @@ def eval_genomes():
         user_input = pygame.key.get_pressed() 
         if sum(pygame.key.get_pressed()) <= 1:
             vehicle.sprite.drive_state = False
+            vehicle.sprite.direction = 0
+        
         
         #Drive 
         if user_input[pygame.K_UP]:
             vehicle.sprite.drive_state = True
+
+        
+        #Steering
+        if user_input[pygame.K_LEFT]:
+            vehicle.sprite.direction = -1
+
+        if user_input[pygame.K_RIGHT]:
+            vehicle.sprite.direction = 1
 
         #update
         vehicle.draw(SCREEN)
